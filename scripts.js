e5f050196a4bd987850cdce9582d0f5d24dcc65f
Amber <a.rignell16@ejm.org>
2020-07-29 18:44:48 +0200

    add checkbox to template list
    Relates #17
    
    Co-authored-by: mhtien <myhoatien@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
+const checkbox = '<input type="checkbox" name="checkbox" id="checkbox">';
+
 function addTemplate(time, text) {
     let template = document.getElementById('template'); // select the template element
     let fragment = template.content.cloneNode(true); //get read-only documentFragment from the template, and clone it to put it into the fragment variable 
-    let testItem = fragment.querySelector(".listItem"); //select list item from the
-    testItem.textContent = text;
-    console.log(fragment);
+    let testItem = fragment.querySelector(".listItem"); //select list item from the fragment
+    testItem.innerHTML = checkbox + text;
+    console.log(fragment.firstChild.content);
     let section = document.querySelector(`.${time}`); // select the section related to the time argument
-    section.appendChild(testItem); //add the new list item to the section
+    section.appendChild(fragment); //add the new list item to the section
 };
+
+addTemplate("today", "My Hoa");
 
 
