01e59fd1ea04a8554116f1f1c3b6e4d1256fea06
Amber <a.rignell16@ejm.org>
2020-07-30 11:21:18 +0200

    Added checkbox to listItems template
    Co-authored-by: mhtien <myhoatien@gmail.com>
    
    Relates #17

--- scripts.js
+++ scripts.js
@@ -4,12 +4,13 @@ function addTemplate(time, text) {
     let template = document.getElementById('template'); // select the template element
     let fragment = template.content.cloneNode(true); //get read-only documentFragment from the template, and clone it to put it into the fragment variable 
     let testItem = fragment.querySelector(".listItem"); //select list item from the fragment
-    testItem.innerHTML = checkbox + text;
-    console.log(fragment.firstChild.content);
+    testItem.childNodes[2].textContent = text;
+    console.log(testItem.childNodes[2]);
     let section = document.querySelector(`.${time}`); // select the section related to the time argument
     section.appendChild(fragment); //add the new list item to the section
-};
+}; 
 
 addTemplate("today", "My Hoa");
+addTemplate("tomorrow", "Amber");
 
 
