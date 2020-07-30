57e50b37ae1798ab2982894f34ffa9ff60fb04d7
mhtien <59407030+mhtien@users.noreply.github.com>
2020-07-30 17:57:25 +0100

    added comments to tests
    Relates to #16
    
    Co-authored-by: amberrignell <a.rignell16@ejm.org>
    
    Co-authored-by: Lisa Cernilogar <lisacernilogar@me.com>
    
    Co-authored-by: Josh <joshua.hart@protonmail.com>

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const addTemplate = () => {
   let fragment = template.content.cloneNode(true); //get read-only documentFragment from the template, and clone it to put it into the fragment variable
   let testItem = fragment.querySelector(".listItem"); //select list item from the
   testItem.childNodes[2].textContent = activity.value;
-  let section = document.querySelector(`.${time.value}`); // select the section related to the time argument
+  let section = document.querySelector(`.${time.value}`); // select the unordered list in the relevant section related to the time argument
   section.appendChild(fragment); //add the new list item to the section
 };
 
