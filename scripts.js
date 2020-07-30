d8d2a3bbc6d67fa5888dbe5469a84e6711b2ae7b
mhtien <59407030+mhtien@users.noreply.github.com>
2020-07-30 17:00:30 +0100

    CSS Styling
    Relates to #6 and #25
    
    Co-authored-by: amberrignell <a.rignell16@ejm.org>
    
    Co-authored-by: Lisa Cernilogar <lisacernilogar@me.com>
    
    Co-authored-by: Josh <joshua.hart@protonmail.com>

--- scripts.js
+++ scripts.js
@@ -2,29 +2,30 @@ let activity = document.querySelector("#activity-select");
 let time = document.querySelector("#date-select");
 
 const addTemplate = () => {
-    let template = document.getElementById('template'); // select the template element
-    let fragment = template.content.cloneNode(true); //get read-only documentFragment from the template, and clone it to put it into the fragment variable 
-    let testItem = fragment.querySelector(".listItem"); //select list item from the
-    testItem.childNodes[2].textContent = activity.value;
-    let section = document.querySelector(`.${time.value}`); // select the section related to the time argument
-    section.appendChild(fragment); //add the new list item to the section
+  let template = document.getElementById("template"); // select the template element
+  let fragment = template.content.cloneNode(true); //get read-only documentFragment from the template, and clone it to put it into the fragment variable
+  let testItem = fragment.querySelector(".listItem"); //select list item from the
+  testItem.childNodes[2].textContent = activity.value;
+  let section = document.querySelector(`.${time.value}`); // select the section related to the time argument
+  section.appendChild(fragment); //add the new list item to the section
 };
 
-
 const form = document.querySelector("form");
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-    addTemplate();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  addTemplate();
+  form.reset();
 });
 
 //Function to delete items in the list when the delete button is clicked
-document.addEventListener("click", (e) => { //when the document is clicked
-    if (e.target.className === 'delete-button') { //if the item clicked has a class of 'delete-button'
-        e.target.parentNode.remove(); //remove its parent node (the entire list item)
-    }
+document.addEventListener("click", (e) => {
+  //when the document is clicked
+  if (e.target.className === "delete-button") {
+    //if the item clicked has a class of 'delete-button'
+    e.target.parentNode.remove(); //remove its parent node (the entire list item)
+  }
 
-    if (e.target.className === "checkbox") {
-        e.target.parentNode.classList.toggle("strikethrough");
-    }
+  if (e.target.className === "checkbox") {
+    e.target.parentNode.classList.toggle("strikethrough");
+  }
 });
-
